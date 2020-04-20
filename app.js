9a521be13068a33bbea777ccc867dd0a0cc885b6
Christina Leung <cgleungsf@gmail.com>
2020-04-19 17:00:46 -0700

    added questions and command line responses

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,28 +8,96 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("../lib/htmlRenderer");
+const team = [];
+const render = require("./lib/htmlRenderer");
 
-const teamManagerQuestions = {}
-const teamMemberQuestions = {}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function promptUser() {
-    return inquirer.prompt(
-        teamManagerQuestions,
-        teamMemberQuestions
-
+function initPrompt() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What position would you like to add?",
+            choices: ["Manager", "Engineer", "Intern", "I'm done, I don't want to add any more team members"],
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Enter employee name: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter employee email: "
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter employee id: "
+        }
+    ],
     ).then(async function (data) {
-        let teamInfo = { data }
-        console.log(teamInfo)
+        // let specific = await addPrompt(data);
+        await addPrompt(data);
+
     })
+}
+
+async function addPrompt(response) {
+    if (response.role === "Manager") {
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Enter employee office number",
+            },
+        ]).then(async function (data) {
+            const managerInput = { ...response, ...data };
+            const manager = new Manager(managerInput.name, managerInput.role, managerInput.email, managerInput.id, managerInput.officeNumber);
+            team.push(manager);
+            initPrompt()
+        })
+
+
+    } else if (response.role === "Engineer") {
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "github",
+                message: "Enter employee GitHub username",
+            }
+        ]).then(async function (data) {
+            const engineerInput = { ...response, ...data };
+            const engineer = new Engineer(engineerInput.name, engineerInput.role, engineerInput.email, engineerInput.id, engineerInput.github);
+            team.push(engineer);
+            initPrompt()
+        })
+    } else if (response.role === "Intern") {
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "school",
+                message: "Enter employee school name",
+            }
+        ]).then(async function (data) {
+            const internInput = { ...response, ...data };
+            const intern = new Intern(internInput.name, internInput.role, internInput.email, internInput.id, internInput.school);
+            team.push(intern);
+            initPrompt()
+        })
+    }
+    else {
+        console.log(team);
+        console.log("no more team members to add")
+    }
 }
 
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-render();
+// render();
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -49,7 +117,7 @@ render();
 
 async function init() {
     try {
-         await promptUser();
+        await initPrompt();
     } catch (err) {
         console.log("Error alert:" + err);
     }
